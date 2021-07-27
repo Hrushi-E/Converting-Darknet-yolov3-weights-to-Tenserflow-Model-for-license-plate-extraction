01dd3ff3e4b7499d48a159aa788b0db81884ab58
EtikikotaHrushikesh <69776196+EtikikotaHrushikesh@users.noreply.github.com>
2021-07-27 22:28:19 +0530

    cropping and LicensePlate.txt added

--- core/functions.py
+++ core/functions.py
@@ -57,7 +57,84 @@ def crop_objects(img, data, path, allowed_classes):
             cv2.imwrite(img_path, cropped_img)
         else:
             continue
-        
+
+            
+# function for cropping each detection and saving as new image
+def crop_objects(img, data, path, allowed_classes):
+    boxes, scores, classes, num_objects = data
+    class_names = read_class_names(cfg.YOLO.CLASSES)
+    #create dictionary to hold count of objects for image name
+    counts = dict()
+    # mine
+    for i in range(num_objects):
+        p=[];lp=[];h=[];nh=[];mc=[];mcn=[];pwoh=[];temp=0
+        # get count of class for part of image name
+        class_index = int(classes[i])
+        class_name = class_names[class_index]
+        xmin, ymin, xmax, ymax = boxes[i]
+        if class_name =='Person':
+            p.append([xmin, ymin, xmax, ymax])
+        elif class_name =='Helmet':
+            h.append([xmin, ymin, xmax, ymax])
+        elif class_name=='Vehicle_reg_number':
+            lp.append([xmin, ymin, xmax, ymax])
+        m=len(p)
+        n=len(h)
+        o=len(lp)
+        e=len(nh)
+        f=len(mc)
+        for j in range(e):
+            c0=p[j][0];d0=p[j][1];c1=p[j][2];d1=p[j][3];
+            for k in range(m):
+                x0==nh[k][0]
+                y0==nh[k][1]
+                x1==nh[k][2]
+                y1==nh[k][3]
+                if (x0<c0 and y0<d1)and(x1>c1 and y1>d1):
+                    pwoh.append([x0,y0,x1,y1])
+                    break
+
+        s=len(pwoh)
+        for k in range(s):
+            x0=pwoh[k][0]; y0=pwoh[k][1]; x1=pwoh[k][2]; y1=pwoh[k][3]; 
+            for j in range(f):
+                a0=lp[j][0]
+                b0=lp[j][1]
+                a1=lp[j][2]
+                b1=lp[j][3]
+
+                # If one rectangle is on left side of other
+                if(x0 >= a1 or a0 >= x1):
+                    continue
+                # If one rectangle is above other
+                if(y0 <= b1 or b0 <= y1):
+                    continue
+                else:
+                    mcn.append([a0,b0,a1,b1])
+ 
+
+        #detection of lp without helmet
+        g=len(mcn)
+        for k in range(g):
+            x0=mcn[k][0];y0=mcnh[k][1];x1=mcn[k][2];y1=mcn[k][3];
+            for j in range(o):
+                a0=lp[j][0]
+                b0=lp[j][1]
+                a1=lp[j][2]
+                b1=lp[j][3]
+                if (a0<x0 and b0<y0) and (a1<x1 and b0<y1):
+                    temp=1
+                    xmin=a0;ymin=b0;xmax=a1;ymax=b1
+                    # crop detection from image (take an additional 5 pixels around all edges)
+                    cropped_img = img[int(ymin)-5:int(ymax)+5, int(xmin)-5:int(xmax)+5]
+                    # construct image name and join it to path for saving crop properly
+                    img_name = class_name + '_' + str(counts[class_name]) + '.png'
+                    img_path = os.path.join(path, img_name)
+                    # save image
+                    cv2.imwrite(img_path, cropped_img)
+
+                    
+
 # function to run general Tesseract OCR on any detections 
 def ocr(img, data):
     boxes, scores, classes, num_objects = data
@@ -82,5 +159,9 @@ def ocr(img, data):
         try:
             text = pytesseract.image_to_string(blur, config='--psm 11 --oem 3')
             print("Class: {}, Text Extracted: {}".format(class_name, text))
+            with open('LicensePlate.txt', 'w') as f:
+                f.write(text)
+                f.write('\n')
+                f.close()
         except: 
             text = None
