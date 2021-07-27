3e66a20c99869e66975d9fc92230abfeed835486
EtikikotaHrushikesh <69776196+EtikikotaHrushikesh@users.noreply.github.com>
2021-07-27 22:31:13 +0530

    Added tesseract path

--- core/utils.py
+++ core/utils.py
@@ -10,7 +10,9 @@ import re
 # If you don't have tesseract executable in your PATH, include the following:
 # pytesseract.pytesseract.tesseract_cmd = r'<full_path_to_your_tesseract_executable>'
 # Example tesseract_cmd = r'C:\Program Files (x86)\Tesseract-OCR\tesseract'
-
+pytesseract.pytesseract.tesseract_cmd = (
+    r'/usr/bin/tesseract'
+)
 # function to recognize license plate numbers using Tesseract OCR
 def recognize_plate(img, coords):
     # separate coordinates from box
